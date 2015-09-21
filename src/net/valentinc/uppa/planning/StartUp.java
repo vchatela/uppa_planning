f808e626f81c594ef4f064ad534f8629a50a8364
Valentin C <valentindu64@hotmail.fr>
2015-09-21 15:49:03 +0200

    Version 2.0
    StartUp page
    Code cleanup
    Update functionalities
    Fix issue

--- src/net/valentinc/uppa/planning/StartUp.java
+++ src/net/valentinc/uppa/planning/StartUp.java
@@ -8,13 +8,13 @@ import android.os.Bundle;
  * Created by valentinc on 21/09/2015.
  * StartUp activity
  */
+
 public class StartUp extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_startup);
-        // wait 1 sec
         new Thread(new Runnable() {
             @Override
             public void run() {
