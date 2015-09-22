1cfd3ebbc7849fd294c1c0f55d4fab5e96d572ef
Valentin C <valentindu64@hotmail.fr>
2015-09-22 14:14:44 +0200

    First Draw test

---
+++ testModule/src/net/valentinc/uppa/planning/test/androidTest.java
@@ -0,0 +1,40 @@
+package net.valentinc.uppa.planning.test;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by valentinc on 22/09/2015.
+ * Generate and Execute android Test after build
+ */
+public class androidTest extends AndroidTestCase {
+    /*
+    // Start the main activity of the application under test
+    mActivity = getActivity();
+
+    // Get a handle to the Activity object's main UI widget, a Spinner
+    mSpinner = (Spinner)mActivity.findViewById(com.android.example.spinner.R.id.Spinner01);
+
+    // Set the Spinner to a known position
+    mActivity.setSpinnerPosition(TEST_STATE_DESTROY_POSITION);
+
+    // Stop the activity - The onDestroy() method should save the state of the Spinner
+    mActivity.finish();
+
+    // Re-start the Activity - the onResume() method should restore the state of the Spinner
+    mActivity = getActivity();
+
+    // Get the Spinner's current position
+    int currentPosition = mActivity.getSpinnerPosition();
+
+    // Assert that the current position is the same as the starting position
+    assertEquals(TEST_STATE_DESTROY_POSITION, currentPosition);
+     */
+
+    /*TODO :
+    - Test spinner instanciation
+    - Test remember value of promo
+    - Test cache works
+    - Test export works
+    - Test StartUp works
+    */
+}
---
+++ testModule/src/net/valentinc/uppa/planning/test/unitTest.java
@@ -0,0 +1,18 @@
+package net.valentinc.uppa.planning.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by valentinc on 22/09/2015.
+ * Generate and Execute unitTest after build
+ */
+
+public class unitTest extends TestCase {
+
+    /*
+    TODO :
+    - cache file works
+    - copy works
+
+     */
+}
