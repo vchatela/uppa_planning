8a86b2e1a7c16ae7b4e6b4736093bfdf5125c411
Valentin C <valentindu64@hotmail.fr>
2015-09-22 18:37:37 +0200

    Add Module Test

---
+++ testModule/src/net/valentinc/uppa/planning/test/unitTest.java
@@ -0,0 +1,27 @@
+package net.valentinc.uppa.planning.test;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by valentinc on 22/09/2015.
+ * Generate and Execute unitTest after build
+ */
+
+public class unitTest extends TestCase {
+
+    /*
+    TODO :
+    - cache file works
+    - copy works
+
+     */
+    @Test
+    public final void testCacheFile() {
+
+    }
+    @Test
+    public final void testCopy() {
+
+    }
+}
